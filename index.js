3f2ef71a507c82afc4822820012036450647e57b
kengurek <kengurek@gmail.com>
2017-01-02 01:07:52 +0300

    add audio player

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 const exec = require('child_process').exec;
+const spawn = require('child_process').spawn;
+const killProcess = require('./utils/').killProcess;
 
 const CONFIG = JSON.parse(fs.readFileSync('./config.json'));
 
@@ -9,9 +11,19 @@ var token = CONFIG['BOT_TOKEN'];
 // Setup polling way
 var bot = new TelegramBot(token, {
     polling: {
-        interval: 10000
+        interval: 1000
     }
 });
+
+var audiosArr = [];
+onAudiosArrayUpdate();
+fs.watch('./audios', {}, onAudiosArrayUpdate);
+function onAudiosArrayUpdate() {
+    audiosArr = fs.readdirSync('./audios/').filter(track => track.indexOf('.mp3') > -1);
+}
+
+var ls;
+var currentIndex = -1;
 
 bot.on('message', function (msg) {
     var chatId = msg.chat.id;
@@ -32,9 +44,7 @@ bot.on('message', function (msg) {
         });
 
         bot.sendChatAction(chatId, 'upload_photo');
-    }
-
-    if (msg.text === '/video') {
+    } else if (msg.text === '/video') {
 
         exec('sh ./capture_video.sh', function (error, stdout, stderr) {
             if (error) {
@@ -50,6 +60,75 @@ bot.on('message', function (msg) {
         });
 
         bot.sendChatAction(chatId, 'upload_video');
+    } else if (msg.text === '/audio') {
+        sendAudiosAvailable(chatId);
+    } else if (!isNaN(msg.text.substr(1))) {
+        play(Number(msg.text.substr(1)) - 1, chatId);
+    } else if (msg.text == 'ПРЕДЫДУЩАЯ') {
+        play(currentIndex - 1, chatId);
+    }
+    else if (msg.text == 'СЛЕДУЮЩАЯ') {
+        play(currentIndex + 1, chatId);
+    } else if (msg.text == 'СТОП') {
+        if (ls) {
+            killProcess(ls.pid);
+        }
+        sendAudiosAvailable(chatId);
+    }
+});
+
+function sendAudiosAvailable(chatId) {
+    var commands = [];
+    var keyboard = [];
+    var row = [];
+    var message = '';
+    audiosArr.forEach((track, index) => {
+        const trackName = track.replace(/[0-9]+\-/, '').replace('.mp3', '');
+        var command = '/' + (index + 1);
+        commands.push(command);
+        message += command + ' ' + trackName + '\n';
+    })
+
+    commands.forEach(function (command) {
+        row.push(command);
+        if (row.length == 10) {
+            keyboard.push(row);
+            row = [];
+        }
+    })
+    keyboard.push(row);
+
+    bot.sendMessage(chatId, message, {parse_mode: 'HTML', reply_markup: {keyboard: keyboard}});
+}
+
+function play(index, chatId) {
+    if (index < 0) {
+        currentIndex = -1;
+        return;
+    }
+    if (index >= audiosArr.length) {
+        currentIndex = audiosArr.length;
+        return;
+    }
+    const track = audiosArr[index];
+    if (!track) {
+        return;
+    }
+    currentIndex = index;
+    const trackName = track.replace(/[0-9]+\-/, '').replace('.mp3', '');
+    const nav = [];
+    if (currentIndex > 0) {
+        nav.push('ПРЕДЫДУЩАЯ');
+    }
+    if (currentIndex < audiosArr.length - 1) {
+        nav.push('СЛЕДУЮЩАЯ');
     }
 
-});
+    bot.sendMessage(chatId, trackName, {reply_markup: {keyboard: [nav, ['СТОП']]}});
+
+    if (ls) {
+        killProcess(ls.pid);
+    }
+
+    ls = spawn('/usr/bin/omxplayer', ['./audios/' + track]);
+}
---
+++ utils/index.js
@@ -0,0 +1,31 @@
+const psTree = require('ps-tree');
+
+module.exports.killProcess = function (pid, signal, callback) {
+    signal = signal || 'SIGKILL';
+    callback = callback || function () {
+        };
+    var killTree = true;
+    if (killTree) {
+        psTree(pid, function (err, children) {
+            [pid].concat(
+                children.map(function (p) {
+                    return p.PID;
+                })
+            ).forEach(function (tpid) {
+                try {
+                    process.kill(tpid, signal)
+                }
+                catch (ex) {
+                }
+            });
+            callback();
+        });
+    } else {
+        try {
+            process.kill(pid, signal)
+        }
+        catch (ex) {
+        }
+        callback();
+    }
+};
